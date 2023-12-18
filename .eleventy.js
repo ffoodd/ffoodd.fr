93b9d4f527a05bbdcc601e4518912a3ea382ce7c
Gaël Poupard <gael+github@ffoodd.fr>
2023-12-18 15:54:02 +0100

    feat(11ty): adding microtypo plugin and a @todo roadmap

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,32 @@
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy("_site/images")
+
+	// @note Micro-typographie : des choses à ajouter ?
+	// @note À utiliser dans les titres et listes, surtout pas sur le contenu.
+	eleventyConfig.addFilter('micro-typo', (text) => {
+		return text
+			.replace(' :', '&nbsp;:')
+			.replace(' ?', ' ?')
+			.replace(' !', ' !')
+			.replace(' ;', ' ;')
+			.replace(' »', ' »')
+			.replace('« ', '« ')
+			.replace(' — ', ' — ')
+			.replace(' / ', ' / ')
+	})
 
 	// @todo Configuration markdown-it :
 	// @link https://www.alpower.com/tutorials/configuring-footnotes-with-eleventy/
+	// @link https://github.com/valeriangalliat/markdown-it-anchor
+	// @todo RSS
+	// @link https://www.11ty.dev/docs/plugins/rss/
+	// @todo Syntax highlighting
+	// @link https://www.11ty.dev/docs/plugins/syntaxhighlight/
+	// @todo HTML minifier ?
+	// @link https://github.com/terser/html-minifier-terser
+	// @todo Robots.txt
+	// @link https://micro.anniegreens.lol/2023/09/29/updated-my-microblog.html
+
+	eleventyConfig.addPassthroughCopy("_site/images")
 
 	eleventyConfig.setServerOptions({
 		liveReload: true
