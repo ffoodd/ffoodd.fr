da6bdda0b54ecc304fd918e36c2d1a5f83fc6445
Gaël Poupard <gael+github@ffoodd.fr>
2024-09-30 17:45:00 +0200

    feat(devfest.2024): start styles by including sseeeedd

--- docs/devfest.2024/jeu/play-ground.js
+++ docs/devfest.2024/jeu/play-ground.js
@@ -20,6 +20,8 @@ class PlayGround extends HTMLElement {
 						// @todo Lien vers le niveau suivant ? Événement ?
 						// @note Fiche « IRL » avec cas d’usage réél (?)
 						// @note Pour que tout le monde franchisse les paliers en même temps…
+						// @todo Possibilité de rejouer le niveau ?
+						// @todo Présentation du cas d’usage avant de passer au niveau suivant
 						console.log('Success!');
 						this.spyer.disconnect();
 						clearTimeout(this.invader);
@@ -40,6 +42,10 @@ class PlayGround extends HTMLElement {
 		}
 	}
 
+	generateRandomDelay(min = 300, max = 3000) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
+
 	_isMutant(element) {
 		return element.nodeName.toLowerCase() === 'mu-tant';
 	}
@@ -50,13 +56,18 @@ class PlayGround extends HTMLElement {
 
 	_invade() {
 		this.invader = setTimeout(() => {
+			// @todo Ajouter des mutants sans type (humain) à ne pas dézinguer ?
+			// @note Et vérifier qu’ils n’ont pas été tués… ?
+			// @note Voire même pour certains niveaux : ajouter un type après chargement, en mode contagion ?
+			// @todo Un niveau contenant tous les types de mutants, pour en cibler un en particulier ?
+			// @note Pour les niveaux supérieurs ?
+			// @todo Sinon détecter la présence, au lieu de chercher à tuer ?
 			let mutant = document.createElement('mu-tant');
 			mutant.type = this.type;
 			this.append(mutant);
 			this._invade();
 			// @note Ajouter du son : un BIP par apparition, etc.
-		}, 1000);
-		// @todo Random timer ↑
+		}, this.generateRandomDelay());
 	}
 
 	_stopInvasion(options = '"characterData": true, "childList": true', condition = '', type = 'json') {
@@ -84,7 +95,9 @@ class PlayGround extends HTMLElement {
 	_killMutant(mutant, options, condition, wachTextNode) {
 		const hunter = new MutationObserver(mutations => {
 			for (const mutation of mutations) {
+				// @todo Comparaison : solution attendue pour le niveau ?
 				const target = wachTextNode ? mutation.target.parentNode : mutation.target;
+				// @todo Avec callback complet ?
 				if (condition !== '') {
 					eval(`if (${condition}) {
 						target.remove();
